8aee20262603ca73ed039b6ebda4f35c10d2c574
AdityaMishra001 <adiakamishra@gmail.com>
2026-06-11 13:52:47 +0530

    Time: 20 ms (49.38%), Space: 34.6 MB (85.1%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,54 +1,34 @@
 class Solution {
+    using pii = pair<int,int>;
 public:
-    //approach 1
-    //brute force
-    //from every cell search for min distance 0
-    // m*n celss *( search => m*n) ~O((m*n)^2)
-
-    //approach 2
-    //initialize ans with -1
-    //now mark all zeros
-    //now for each zero
-    //update all cells connected to 1(if neighbour is !-1 avoid this path)
-    //now neighbour of 2 are marked 2;
-
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int rows=mat.size();
-        int cols=mat[0].size();
-
-        vector<vector<int>>ans(rows,vector<int>(cols,-1));
-
-        vector<vector<int>>dir={{1,0},{0,-1},{-1,0},{0,1}};
-        queue<pair<int,int>>q;
-
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(mat[i][j]==0){
-                    ans[i][j]=0;
-                    q.push({i,j});
-                }
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>>dist(m, vector<int>(n, -1));
+        queue<pii>q;
+        // push all zeroes
+        for(int i = 0; i <m;i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 0)
+                    q.push({i, j}), dist[i][j] = 0;
             }
         }
+
+        vector<int> dx = {-1, 0, 1, 0};
+        vector<int> dy = {0, -1, 0, 1};
+        // vector<vector<int>>dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        // vector<int> dirs = {-1, 0, 1, 0, -1};
 
         while(!q.empty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                auto[x,y]=q.front();
-                q.pop();
-
-                for(int v=0;v<4;v++){
-                    int xnew=x+dir[v][0];
-                    int ynew=y+dir[v][1];
-
-                    if(xnew<0 || ynew<0 || xnew>=rows || ynew>=cols ||
-                    ans[xnew][ynew]!=-1){
-                        continue;
-                    }
-                    ans[xnew][ynew]=ans[x][y]+1;
-                    q.push({xnew,ynew});
-                }
+            pii p = q.front();
+            q.pop();
+            int i = p.first, j = p.second;
+            for(int d = 0; d < 4; d++){
+                int ni = i + dx[d], nj = j + dy[d];
+                if(ni >= 0 && ni < m && nj >= 0 && nj < n)
+                    if(dist[ni][nj] == -1)
+                        dist[ni][nj] = dist[i][j] + 1, q.push({ni, nj});
             }
         }
-        return ans;
+        return dist;
     }
 };
