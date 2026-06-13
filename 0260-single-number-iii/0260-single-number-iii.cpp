63e68fee2bc2a233a9f00c9899e5c34779e0bd9c
AdityaMishra001 <adiakamishra@gmail.com>
2026-06-13 09:13:14 +0530

    Time: 0 ms (100%), Space: 13.9 MB (55.12%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int eXor=0;
+        int xorr=0,a=0,b=0;
         for(int num:nums){
-            eXor^=num;
+            xorr^=num;
         }
-        int mask;
-        for(int i=0;i<32;i++){
-            if(eXor & (1<<i)){
-                mask=i;
-                break;
-            }
-        }
-        mask=(1<<mask);
+        int mask= xorr & int( -1LL* xorr);
 
-        int Xor=0;
-        eXor=0;
+        
         for(int num:nums){
             if(num&mask){
-                eXor^=num;
+                a^=num;
             }else{
-                Xor^=num;
+                b^=num;
             }
         }
-        return{Xor,eXor};
+        return{a,b};
     }
 };
