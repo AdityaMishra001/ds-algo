df01783b834add93e0011b0c7ed2effbb75fbbff
AdityaMishra001 <adiakamishra@gmail.com>
2026-06-13 17:06:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (88.15%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int D=m-1;
-        int R=n-1;
+        // int D=m-1;
+        // int R=n-1;
 
-        if(R<D)swap(R,D);
+        if(m<n)swap(m,n);
         int ways=1;
-        //considering D to be min
-        for(int i=1;i<=D;i++){
-            ways= int( 1LL*ways* (R+i) / i );
+        //considering n to be min
+        for(int i=1;i<n;i++){
+            ways= int( 1LL*ways* (m-1+i) / i );
         }
 
         return ways;
