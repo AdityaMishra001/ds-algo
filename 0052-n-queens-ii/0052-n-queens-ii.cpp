13acd168f2dc751d49ccdeac389e1b75c93fbf97
AdityaMishra001 <adiakamishra@gmail.com>
2026-07-14 15:06:55 +0530

    Time: 0 ms (100%), Space: 7.7 MB (96.68%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    
-    void solve(vector<bool>&ColAttk,vector<bool>&lDigAttk,vector<bool>&rDigAttk,int row, int n,int &ct){
-        if(row==n){
-            ct++;
-            return;
-        }
+    int solve(int colMask,int leftDigMask, int rightDigMask, int n){
+        int mask= (1<<n)-1;
 
-        
+        if(colMask==mask)return 1;
 
-        for(int col=0;col<n;col++){
-            if(ColAttk[col] || lDigAttk[n-1+col-row] || rDigAttk[row+col])continue;
-            ColAttk[col]=1;
-            lDigAttk[n-1+col-row]=1;
-            rDigAttk[row+col]=1;
-
-            solve(ColAttk,lDigAttk,rDigAttk,row+1,n,ct);
-            
-            ColAttk[col]=0;
-            lDigAttk[n-1+col-row]=0;
-            rDigAttk[row+col]=0;
-
-        }
-    }
-    
-    int totalNQueens(int n) {
         int ct=0;
-        vector<bool>ColAttk(n),lDigAttk(2*n-1),rDigAttk(2*n-1);
-
-        solve(ColAttk,lDigAttk,rDigAttk,0,n,ct);
+        int available= (~(colMask|leftDigMask| rightDigMask)) & mask;
+        while(available){
+            int p=available& -available;//rightmostSetBit
+            ct+= solve( colMask|p,(leftDigMask|p)>>1,(rightDigMask|p)<<1,n );
+            available-=p;
+        }
         return ct;
+
+    }
+    int totalNQueens(int n) {
+        return solve(0,0,0,n);
     }
 };
