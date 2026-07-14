02a29e583f3b178f1f75df54cb18113f86abb3d8
AdityaMishra001 <adiakamishra@gmail.com>
2026-07-14 14:46:51 +0530

    Time: 0 ms (100%), Space: 10.4 MB (41.78%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,44 +1,36 @@
 class Solution {
+    vector<vector<string>>ans;
+    vector<string>board;
+    vector<int>rightDiag,leftDiag,top;
+    int n;
 public:
-    void solve( vector<vector<string>>&ans,vector<string>&board,vector<bool>&AttackFromTop,vector<bool>&AttackFromTopl,vector<bool>&AttackFromTopr,int row,int n){
+    void solve(int row){
         if(row==n){
-            //if all rows 0 to n-1 are filled now at index n save answer
             ans.push_back(board);
             return;
         }
 
-        // at current row search for suitable col
         for(int col=0;col<n;col++){
+            if(rightDiag[row+col] || leftDiag[n-1+col-row] || top[col] )continue;
 
-            //if not possible continue;
-            if(AttackFromTop[col] || AttackFromTopl[n-1+row-col] || AttackFromTopr[row+col])continue;
-            
-            //place the queen at possible place
             board[row][col]='Q';
-            AttackFromTop[col]=1;
-            AttackFromTopl[row-col+n-1]=1;
-            AttackFromTopr[row+col]=1;
-
-            solve(ans,board,AttackFromTop,AttackFromTopl,AttackFromTopr,row+1,n);
-
-            //remove queen
+            rightDiag[row+col] = leftDiag[n-1+col-row] = top[col]=1;
+            solve(row+1);
             board[row][col]='.';
-            AttackFromTop[col]=0;
-            AttackFromTopl[row-col+n-1]=0;
-            AttackFromTopr[row+col]=0;
+            rightDiag[row+col] = leftDiag[n-1+col-row] = top[col]=0;
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
-        vector<string>board;
+        ans.clear(),board.clear();
+        this->n=n;
+        rightDiag.assign(2*n,0),leftDiag.assign(2*n,0),top.assign(n,0);
+
         for(int i=0;i<n;i++){
             string row(n,'.');
             board.push_back(row);
         }
-        vector<bool> AttackFromTop(n),AttackFromTopl(2*n-1),AttackFromTopr(2*n-1);
-        solve(ans,board,AttackFromTop,AttackFromTopl,AttackFromTopr,0,n);
-        //cal helper to generate the ans
-        //call for 0th row 
+
+        solve(0);
         return ans;
     }
 };
